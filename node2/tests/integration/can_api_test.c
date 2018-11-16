97155e9e25a95776f347a694f686f68b10a88bad
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2018-11-16 08:06:34 +0100

    Clean up some tests

--- node2/tests/integration/can_api_test.c
+++ node2/tests/integration/can_api_test.c
@@ -10,18 +10,19 @@ static volatile int CAN_MSG_RECEIVED = 0;
 
 int main() {
     UART_Init(MYUBRR);
-	fdevopen(*UART_Transmit, *UART_Receive);
+    fdevopen(*UART_Transmit, *UART_Receive);
 
     printf("\n\rStarting CAN receive test with INT\n\r");
 
     can_api_init();
     EICRA &= ~(1 << ISC30); // Interrupt on falling edge
-	EICRA |= (1 << ISC31); // Interrupt on falling edge
-	EIMSK |= (1 << INT3); // Enable interrupt on INT3
+  	EICRA |= (1 << ISC31); // Interrupt on falling edge
+  	EIMSK |= (1 << INT3); // Enable interrupt on INT3
 
     sei();
     while(1) {
         printf("Servo position: %d\n\r", Get_servo_reference());
+        _delay_ms(200);
     }
     return 0;
 }
--- node2/tests/integration/servo_test.c
+++ node2/tests/integration/servo_test.c
@@ -1,30 +1,29 @@
-
-
 #include "parameters.h"
+#include "uart.h"
 #include <avr/io.h>
 #include <stdlib.h>
-#include <avr/interrupt.h>
 #include <avr/delay.h>
-#include "uart.h"
-#include "reference_state.h"
+#include <avr/interrupt.h>
 
-#include "ir.h"
+void set_servo_and_wait(int servo_val) {
+    printf("Setting servo to %d\n\r", servo_val);
+    servo_set_pos(servo_val);
+    _delay_ms(500);
+}
 
 int main() {
-	UART_Init(MYUBRR);
-	fdevopen(*UART_Transmit,NULL);
+  	UART_Init(MYUBRR);
+  	fdevopen(*UART_Transmit,NULL);
 
-	PWM_init();
-	sei();
+    PWM_init();
+    sei();
 
-    printf("setting servo\n\r");
-    servo_set_pos(1);
-    _delay_ms(300);
-    servo_set_pos(1);
-    _delay_ms(300);
-    servo_set_pos(0);
-
-    while(1);
+    while(1) {
+        set_servo_and_wait(1);
+        set_servo_and_wait(50);
+        set_servo_and_wait(99);
+        set_servo_and_wait(50);
+    }
 
 	return 0;
 }
